9c0398aa35dfe42b4570aa96e528c86c94defeff
NEKOMENG_LAPTOP\27430 <fallce2024@qq.com>
2026-04-09 09:47:45 +0800

    feat: bander.py现在可以通过命令行参数直接使用了。 -eg1:python3 ./bander.py <ip> -eg2:python3 ./bander.py <pathfile>

--- bander.py
+++ bander.py
@@ -1,9 +1,10 @@
 """
-此段代码用于扫描目标主机是否存在vsftp2.3.4。
-当然，即便没有vsftp2.3.4也可以用于扫描端口。
+对一组IP地址的特点端口进行扫描。
 """
+import os
 import socket
 import time
+import sys
 from ping3 import ping
 
 socket.setdefaulttimeout(2)
@@ -11,6 +12,8 @@ ipaddr = ""
 bander = ""
 ip_list = [] #用于写入IP地址
 port = 0
+mode = 0 #判断是否为命令行模式。
+filepath = None
 
 def s_content(overtime,__ipaddr__,__port__):
     """socket连接"""
@@ -31,25 +34,29 @@ def ipv4_test_1(__ipaddr__):
     return True
 
 def ipv4_test_2(__ipaddr__):
-    #检查地址是否符合IPv4范围
+    #检查地址是否符合IPv4范围（非网络地址或者广播）
     parts = __ipaddr__.split(".")
     for part in parts:
         if not part.isdigit():
-            print("IP地址不合法！正确的范围应为（0~255.0~255.0~255.0~255）")
+            print("IP地址不合法！正确的范围应为（1~254.1~254.1~254.1~254）")
             return False
-        if int(part) > 255 or int(part) < 0:
-            print("IP地址不合法！正确的范围应为（0~255.0~255.0~255.0~255）")
+        if int(part) > 254 or int(part) < 0:
+            print("IP地址不合法！正确的范围应为（0~254.0~254.0~254.0~254）")
             return False
     return True
 
 
-def read_ip():
-    if input("需要使用配置文件吗？（Y）") == "Y":
-        try:
-            f = open("ip.txt", "r")
-        except FileNotFoundError as __e:
-            print(f"发生错误,ip.txt文件不存在！{__e}")
-            exit(-1)
+def read_ip(__filepath__):
+        if not os.path.exists(__filepath__):
+            print(__filepath__,"没有这个文件或者文件无法打开!")
+            sys.exit(-1)
+        else:
+            f = open(__filepath__, "r")
+        # try:
+        #     f = open("ip.txt", "r")
+        # except FileNotFoundError as __e:
+        #     print(f"发生错误,ip.txt文件不存在！{__e}")
+        #     sys.exit(-1)
         for _ in f.readlines():
             addr= _.strip("\n")
             print()
@@ -64,41 +71,54 @@ def read_ip():
             ip_list.append(addr)
             print(f"即将被使用的地址：{ip_list}")
             time.sleep(0.5)
-        return True
-    else:
-        return False
+
 
 
 if __name__ == "__main__":
-    if not read_ip():
-        while True:
-            ipaddr = str(input("IP:"))
-            #用于检查IP是否合法。
-            if not ipv4_test_1(ipaddr):
-                continue
-            if not ipv4_test_2(ipaddr):
-                continue
+    if len(sys.argv) == 2:
+        mode = 1
+        ipaddr = sys.argv[1]
+        if ipv4_test_1(ipaddr) and ipv4_test_2(ipaddr):
+            ip_list.append(ipaddr)
+            print(f"使用命令行参数中的 IP：{ipaddr}")
+        else:
+            read_ip(sys.argv[1])
 
-            if ping(ipaddr,timeout=2) is not None:
-                pass
-            else:
-                ack = input("尝试ping主机失败，如果要继续请输入Y：")
-                if 'Y' in ack:
-                    pass
-                else:
-                    print("已忽略当前输入的IP。")
-                    print(f"当前已被添加的地址：{ip_list}")
+
+
+    if mode != 1:    #如果不是命令行模式，再按照下面流程走。
+        if input("需要使用配置文件吗？（Y）") == "Y":
+            filepath = input("请输入列表路径:")
+            read_ip(filepath)
+        else:
+            while True:
+                ipaddr = str(input("IP:"))
+                #用于检查IP是否合法。
+                if not ipv4_test_1(ipaddr):
+                    continue
+                if not ipv4_test_2(ipaddr):
                     continue
 
-            ip_list.append(ipaddr)
+                if ping(ipaddr,timeout=2) is not None:
+                    pass
+                else:
+                    ack = input("尝试ping主机失败，如果要继续请输入Y：")
+                    if 'Y' in ack:
+                        pass
+                    else:
+                        print("已忽略当前输入的IP。")
+                        print(f"当前已被添加的地址：{ip_list}")
+                        continue
 
-            ack = input("需要继续添加IP地址吗？（Y）")
-            if 'Y' in ack:
-                print(f"当前已被添加的地址：{ip_list}")
-                continue
-            else:
-                print(f"即将被使用的地址：{ip_list}")
-                break
+                ip_list.append(ipaddr)
+
+                ack = input("需要继续添加IP地址吗？（Y）")
+                if 'Y' in ack:
+                    print(f"当前已被添加的地址：{ip_list}")
+                    continue
+                else:
+                    print(f"即将被使用的地址：{ip_list}")
+                    break
 
     while True:
         try:
@@ -131,5 +151,5 @@ if __name__ == "__main__":
         print(f"{ipaddr}:{port}结束!")
         print()
 
-    exit(0)
+    sys.exit(0)
 
